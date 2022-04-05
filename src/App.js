dac27d3a1d08cf9f6f23a60e51e399c3ff90774d
Yovana Pelayo <yovana_pelayo@yahoo.com>
2022-04-05 14:48:28 -0700

    moved state from apps js and got executePassbutton to half work

--- src/App.js
+++ src/App.js
@@ -10,11 +10,9 @@ function App() {
     playerOneHand,
     playerTwoHand,
     playerThreeHand,
-    setPlayerOneHand,
-    setPlayerTwoHand,
-    setPlayerThreeHand,
+
     to,
-    setDeck,
+
     from,
     deck,
     setSelectedCard,
@@ -22,31 +20,6 @@ function App() {
 
     selectedCard,
   } = useGameContext();
-  function findCardIndex(value, suit, cards) {
-    return cards.findIndex((card) => card.value === value && card.suit === suit);
-  }
-
-  function passCard(card) {
-    const playerHands = [playerOneHand, playerTwoHand, playerThreeHand];
-    const playerHandSetFunctions = [setPlayerOneHand, setPlayerTwoHand, setPlayerThreeHand];
-
-    // arrays start at zero, but our players start at 1 :shrug:
-    const toHand = playerHands[to - 1] || deck;
-    const fromHand = playerHands[from - 1] || deck;
-
-    const toSetFunction = playerHandSetFunctions[to - 1] || setDeck;
-    const fromSetFunction = playerHandSetFunctions[from - 1] || setDeck;
-
-    const cardToMoveIndex = findCardIndex(card.value, card.suit, fromHand);
-    const [cardToMove] = fromHand.splice(cardToMoveIndex, 1);
-
-    toHand.push(cardToMove);
-
-    toSetFunction([...toHand]);
-    fromSetFunction([...fromHand]);
-
-    setSelectedCard(null);
-  }
 
   return (
     <div className="App">
@@ -63,18 +36,7 @@ function App() {
           player={'deck'}
         />
       </section>
-      <section>
-        {selectedCard && (
-          <ExecutePassButton
-            passCard={passCard}
-            setFrom={setFrom}
-            from={from}
-            to={to}
-            selectedCard={selectedCard}
-            setSelectedCard={setSelectedCard}
-          />
-        )}
-      </section>
+      <section>{selectedCard && <ExecutePassButton />}</section>
     </div>
   );
 }
--- src/components/Player.js
+++ src/components/Player.js
@@ -1,21 +1,16 @@
 import React from 'react';
+import { useGameContext } from '../context/GameContext';
 import CardList from './CardList';
 
-export default function Player({
-  player,
-  setTo,
-  hand,
-  setSelectedCard,
-
-  to,
-}) {
+export default function Player({ player, hand }) {
+  const { to, setTo } = useGameContext();
   return (
     <div
       className={`player ${to === player ? 'selected-player' : ''}`}
       onClick={() => setTo(player)}
     >
       <p>Player {player}</p>
-      <CardList player={player} cards={hand} setSelectedCard={setSelectedCard} />
+      <CardList player={player} cards={hand} />
     </div>
   );
 }
